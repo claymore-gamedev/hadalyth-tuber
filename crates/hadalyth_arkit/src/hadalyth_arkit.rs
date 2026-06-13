31a81458e2f1546856c295aa95b0a5dccbc92571
CLAYMORE-DEV <claymore.gamedev@gmail.com>
2026-06-13 04:25:53 -0400

    Process only newest frame

--- crates/hadalyth_arkit/src/hadalyth_arkit.rs
+++ crates/hadalyth_arkit/src/hadalyth_arkit.rs
@@ -5,6 +5,7 @@ use godot::prelude::*;
 use crate::custom_config::hadalyth_arkit_connection::HadalythArkitConnection;
 use crate::custom_events::arkit_event::ArkitEvent;
 use crate::custom_resources::live_link_face_blend_shapes::LiveLinkFaceBlendShapes;
+use crate::custom_resources::live_link_face_payload::LiveLinkFacePayload;
 use crate::hadalyth_arkit_async::{get_local_ip_address_async, open_udp_socket_async};
 
 #[derive(GodotClass)]
@@ -65,6 +66,8 @@ impl INode for HadalythArkit {
 
     fn process(&mut self, delta: f64) {
         // Parse callbacks from rx
+        let mut live_link_face_payload : Option<LiveLinkFacePayload> = None;
+        
         while let Ok(event) = self.rx.try_recv() {
             match event {
                 // Startup
@@ -81,62 +84,63 @@ impl INode for HadalythArkit {
 
                 // ARKit packets parsed into useful parameters
                 ArkitEvent::ArkitPayload(payload) => {
-                    if godot::classes::Os::singleton().has_feature("arkit_debug") {
-                        godot_print!("ArkitEvent::ArkitPayload:");
-                        godot_print!("\tversion:{}", payload.version);
-                        godot_print!("\tuuid:{}", payload.uuid);
-                        godot_print!("\tname:{}", payload.name);
-                        godot_print!("\tframe_number:{}", payload.frame_number);
-                        godot_print!("\tsub_frame:{}", payload.sub_frame);
-                        godot_print!("\tfps:{}", payload.fps);
-                        godot_print!("\tdenominator:{}", payload.denominator);
-                        godot_print!("\tblend_shapes:{:?}", payload.blend_shapes);
-                    }
-                    
-                    let blend_shapes = PackedArray::<f32>::from(payload.blend_shapes[0..52].to_vec());
-        
-                    let head_yaw : f32 = payload.blend_shapes[52];
-                    let head_pitch : f32 = payload.blend_shapes[53];
-                    let head_roll : f32 = payload.blend_shapes[54];
-                    let left_eye_yaw : f32 = payload.blend_shapes[55];
-                    let left_eye_pitch : f32 = payload.blend_shapes[56];
-                    let left_eye_roll : f32 = payload.blend_shapes[57];
-                    let right_eye_yaw : f32 = payload.blend_shapes[58];
-                    let right_eye_pitch : f32 = payload.blend_shapes[59];
-                    let right_eye_roll : f32 = payload.blend_shapes[60];
-
-                    // Convert this into a ref counted and emit it to godot
-                    let live_link_face_blend_shapes =
-                        LiveLinkFaceBlendShapes::create(
-                            blend_shapes,
-                            head_yaw,
-                            head_pitch,
-                            head_roll,
-                            left_eye_yaw,
-                            left_eye_pitch,
-                            left_eye_roll,
-                            right_eye_yaw,
-                            right_eye_pitch,
-                            right_eye_roll
-                        );
-
-                    self.signals()
-                        .recv_live_link_face_blend_shapes()
-                        .emit(&live_link_face_blend_shapes);
+                    live_link_face_payload = Some(payload);
                 }
             }
+        }
+
+        match live_link_face_payload {
+            Some(payload) => {
+                if godot::classes::Os::singleton().has_feature("arkit_debug") {
+                    godot_print!("ArkitEvent::ArkitPayload:");
+                    godot_print!("\tversion:{}", payload.version);
+                    godot_print!("\tuuid:{}", payload.uuid);
+                    godot_print!("\tname:{}", payload.name);
+                    godot_print!("\tframe_number:{}", payload.frame_number);
+                    godot_print!("\tsub_frame:{}", payload.sub_frame);
+                    godot_print!("\tfps:{}", payload.fps);
+                    godot_print!("\tdenominator:{}", payload.denominator);
+                    godot_print!("\tblend_shapes:{:?}", payload.blend_shapes);
+                }
+                    
+                let blend_shapes = PackedArray::<f32>::from(payload.blend_shapes[0..52].to_vec());
+
+                let head_yaw : f32 = payload.blend_shapes[52];
+                let head_pitch : f32 = payload.blend_shapes[53];
+                let head_roll : f32 = payload.blend_shapes[54];
+                let left_eye_yaw : f32 = payload.blend_shapes[55];
+                let left_eye_pitch : f32 = payload.blend_shapes[56];
+                let left_eye_roll : f32 = payload.blend_shapes[57];
+                let right_eye_yaw : f32 = payload.blend_shapes[58];
+                let right_eye_pitch : f32 = payload.blend_shapes[59];
+                let right_eye_roll : f32 = payload.blend_shapes[60];
+
+                // Convert this into a ref counted and emit it to godot
+                let live_link_face_blend_shapes =
+                    LiveLinkFaceBlendShapes::create(
+                        blend_shapes,
+                        head_yaw,
+                        head_pitch,
+                        head_roll,
+                        left_eye_yaw,
+                        left_eye_pitch,
+                        left_eye_roll,
+                        right_eye_yaw,
+                        right_eye_pitch,
+                        right_eye_roll
+                    );
+
+                self.signals()
+                    .recv_live_link_face_blend_shapes()
+                    .emit(&live_link_face_blend_shapes);
+                }
+            None => {}
         }
 
         // See if the runtime was spawned correctly
         let Some(ref runtime) = self.runtime else {
             return;
         };
-
-        // See if the connection resource is set
-        let Some(ref hadalyth_arkit_connection) = self.hadalyth_arkit_connection else {
-            return;
-        };
-        let port = hadalyth_arkit_connection.bind().port;
 
         // Attempt to get the local ip address on a timer if it's not available
         let Some(ref local_ip_address) = self.local_ip_address else {
@@ -166,6 +170,11 @@ impl INode for HadalythArkit {
             return;
         }
         self.udp_socket_open = true;
+
+        let Some(ref hadalyth_arkit_connection) = self.hadalyth_arkit_connection else {
+            return;
+        };
+        let port = hadalyth_arkit_connection.bind().port;
 
         let addr: String = format!("{}:{}", local_ip_address, port);
         godot_print!("{}", addr);
